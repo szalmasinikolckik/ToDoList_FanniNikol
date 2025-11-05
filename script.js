759f637639dd0fc6e798a1c79a26734c3fe4ebbc
fulep.fanni <fulep.fanni@ckik.hu>
2025-11-05 10:33:10 +0100

    delete, css

--- script.js
+++ script.js
@@ -5,7 +5,21 @@ function addTask() {
     const li = document.createElement("li");
     li.textContent = input.value;
     ul.appendChild(li);
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "Törlés";
+    li.appendChild(deleteBtn);
+    ul.appendChild(li);
+    deleteBtn.addEventListener("click", () => 
+    {
+        li.remove();
+    });
+
     input.value = "";
 }
 
 
+function saveTask() {
+    const tasks = [];
+    
+}
