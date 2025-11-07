5f4484c031479f0831168bf460778a0efc968e4b
fulep.fanni <fulep.fanni@ckik.hu>
2025-11-07 15:34:08 +0100

    savetask() loadtask()

--- script.js
+++ script.js
@@ -2,28 +2,59 @@ const input = document.getElementById("taskInput");
 const ul = document.querySelector("#taskList");
 
 function addTask() {
-    const li = document.createElement("li");
-    li.textContent = input.value;
-    ul.appendChild(li);
 
-    const deleteBtn = document.createElement("button");
-    deleteBtn.innerText = "Törlés";
-    li.appendChild(deleteBtn);
-    ul.appendChild(li);
+    if(input.value){
+        const li = document.createElement("li");
+    
+
+        const span = document.createElement("span");
+        span.innerText = input.value;
+        li.appendChild(span);
+
+        const deleteBtn = document.createElement("button");
+        deleteBtn.innerText = "Törlés";
+        li.appendChild(deleteBtn);
+        ul.appendChild(li);
+        deleteTask(deleteBtn, li);
+
+        input.value = "";
+        saveTask()
+    }
+
+    else{
+        alert("Kérlek írj valami teendőt!")
+    }
+
+}
+
+function deleteTask(deleteBtn, li){
     deleteBtn.addEventListener("click", () => 
     {
         li.remove();
+        saveTask()
     });
-
-    input.value = "";
 }
 
 function saveTask() {
-    const tasks = [];
+    let tasks = [];
+    const span = ul.querySelectorAll("span");
+
+    for (let i = 0; i < span.length; i++) {
+        tasks.push(span[i].innerText)
+    }
+    
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function loadTask() {
+    const savedTasks = JSON.parse(localStorage.getItem("tasks")); 
+    for (let i = 0; i < savedTasks.length; i++) {
+        input.value = savedTasks[i];
+        addTask();
+
+    }
     
 }
 
-function saveTask() {
-    const tasks = [];
-    
-}
+loadTask();
+
